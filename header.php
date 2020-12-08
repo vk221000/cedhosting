872ae3a79692a7ca967790afb11a8c812b3514c6
vivek kushwaha <vk221000@gmail.com>
2020-12-08 09:45:01 +0530

    third commit

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+$actual_link = $_SERVER["PHP_SELF"];
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
@@ -28,59 +32,63 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <script type="text/javascript" src="js/jquery.hoverdir.js"></script>	
 						<script type="text/javascript">
 							$(function() {
-							
 								$(' #da-thumbs > li ').each( function() { $(this).hoverdir(); } );
-
 							});
 						</script>						
 <!--script-->
 <link rel="stylesheet" href="css/customcss.css">
 </head>
 <body>
-	<!---header--->
-		<div class="header">
-			<div class="container">
-				<nav class="navbar navbar-default">
-					<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
-						<div class="navbar-header">
-							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-								<i class="sr-only">Toggle navigation</i>
-								<i class="icon-bar"></i>
-								<i class="icon-bar"></i>
-								<i class="icon-bar"></i>
-							</button>				  
-							<div class="navbar-brand">
-								<h1><a href="index.php"><span class="logo-color-left">Ced</span><span class="logo-color-right">Hosting</span></a></h1>
-							</div>
-						</div>
+<!---header--->
+<div class="header">
+	<div class="container">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+	<!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<i class="sr-only">Toggle navigation</i>
+						<i class="icon-bar"></i>
+						<i class="icon-bar"></i>
+						<i class="icon-bar"></i>
+					</button>				  
+					<div class="navbar-brand">
+						<h1><a href="index.php"><span class="logo-color-left">Ced</span><span class="logo-color-right">Hosting</span></a></h1>
+					</div>
+				</div>
 
-			<!-- Collect the nav links, forms, and other content for toggling -->
-						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-							<ul class="nav navbar-nav">
-								<li class="active"><a href="index.php">Home <i class="sr-only">(current)</i></a></li>
-								<li><a href="about.php">About</a></li>
-								<li><a href="services.php">Services</a></li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hosting<i class="caret"></i></a>
-									<ul class="dropdown-menu">
-										<li><a href="linuxhosting.php">Linux hosting</a></li>
-										<li><a href="wordpresshosting.php">WordPress Hosting</a></li>
-										<li><a href="windowshosting.php">Windows Hosting</a></li>
-										<li><a href="cmshosting.php">CMS Hosting</a></li>
-									</ul>			
-								</li>
-								<li><a href="pricing.php">Pricing</a></li>
-								<li><a href="blog.php">Blog</a></li>
-								<li><a href="contact.php">Contact</a></li>
-								<li><a href="#">Cart</a></li>
-								<li><a href="login.php">Login</a></li>
-								<li><a href="#">Logout</a></li>
-							</ul>
-									  
-						</div><!-- /.navbar-collapse -->
-					</div><!-- /.container-fluid -->
-				</nav>
-			</div>
-		</div>
-	<!---header--->
+	<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class=<?php echo ($actual_link=='/cedhosting/index.php')? "active" : "" ?>><a href="index.php">Home <i class="sr-only">(current)</i></a></li>
+						<li class=<?php echo ($actual_link=='/cedhosting/about.php')? "active" : "" ?>><a href="about.php">About</a></li>
+						<li class=<?php echo ($actual_link=='/cedhosting/services.php')? "active" : "" ?>><a href="services.php">Services</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hosting<i class="caret"></i></a>
+							<ul class="dropdown-menu">
+								<li class=<?php echo ($actual_link=='/cedhosting/linuxhosting.php')? "active" : "" ?>><a href="linuxhosting.php">Linux hosting</a></li>
+								<li class=<?php echo ($actual_link=='/cedhosting/wordpresshosting.php')? "active" : "" ?>><a href="wordpresshosting.php">WordPress Hosting</a></li>
+								<li class=<?php echo ($actual_link=='/cedhosting/windowshosting.php')? "active" : "" ?>><a href="windowshosting.php">Windows Hosting</a></li>
+								<li class=<?php echo ($actual_link=='/cedhosting/cmshosting.php')? "active" : "" ?>><a href="cmshosting.php">CMS Hosting</a></li>
+							</ul>			
+						</li>
+						<li class=<?php echo ($actual_link=='/cedhosting/pricing.php')? "active" : "" ?>><a href="pricing.php">Pricing</a></li>
+						<li class=<?php echo ($actual_link=='/cedhosting/blog.php')? "active" : "" ?>><a href="blog.php">Blog</a></li>
+						<li class=<?php echo ($actual_link=='/cedhosting/contact.php')? "active" : "" ?>><a href="contact.php">Contact</a></li>
+						<li><a href="#">Cart</a></li>
+						<?php
+						if (isset($_SESSION['user'])) {
+							echo '<li><a href="#">Logout</a></li>';
+						}
+						else {
+							$active_link=($actual_link=="/cedhosting/login.php" ? "active" : "");
+							echo '<li class='.$active_link.'><a href="login.php">Login</a></li>';
+						}
+						?>
+					</ul>	
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container-fluid -->
+		</nav>
+	</div>
+</div>
+<!---header--->
